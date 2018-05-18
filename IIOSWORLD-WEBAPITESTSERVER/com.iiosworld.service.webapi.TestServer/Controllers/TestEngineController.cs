1133d8186c3b533c8618f9aab588f9581d4692ba
Dany St-Laurent <dany.stlaurent1@gmail.com>
2018-05-17 22:48:21 -0400

    New code for testing.

---
+++ IIOSWORLD-WEBAPITESTSERVER/com.iiosworld.service.webapi.TestServer/Controllers/TestEngineController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using com.iiosworld.service.webapi.models;
+
+namespace com.iiosworld.service.webapi.TestServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestEngineController : Controller
+    {
+        // GET api/TestEngine
+        [HttpGet]
+        public IEnumerable<UniqueIdentifier> Get()
+        {
+            const int MAX_IDS = 10;
+            return GenerateIds(MAX_IDS);
+        }
+
+        // GET api/TestEngine/5
+        [HttpGet("{pNumIds}")]
+        public IEnumerable<UniqueIdentifier> Get(int pNumIds)
+        {
+            const int MIN_ID_BATCH = 1;
+            const int MAX_ID_BATCH = 500;
+            const string P1NAME = @"pNumIds";
+            const string P1MSG = @"The batch size must be between 1 and 500.";
+
+            if(pNumIds <= MIN_ID_BATCH || pNumIds > MAX_ID_BATCH)
+            {
+                throw new ArgumentOutOfRangeException(P1NAME, P1MSG);
+            }
+
+            return GenerateIds(pNumIds);
+        }
+        private IEnumerable<UniqueIdentifier> GenerateIds(int pNumIds)
+        {
+            UniqueIdentifier[] newIds = new UniqueIdentifier[pNumIds];
+
+            for (int i = 0; i < pNumIds; i++)
+            {
+                newIds[i] = new UniqueIdentifier();
+            }
+            return newIds.ToList<UniqueIdentifier>();
+        }
+        // POST api/TestEngine
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/TestEngine/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/TestEngine/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ IIOSWORLD-WEBAPITESTSERVER/com.iiosworld.service.webapi.models/UniqueIdentifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace com.iiosworld.service.webapi.models
+{
+    public class UniqueIdentifier
+    {
+        private string _idValue = null;
+
+        public UniqueIdentifier()
+        {
+            if(this._idValue==null)
+            {
+                this._idValue = Guid.NewGuid().ToString();
+            }
+        }
+        public UniqueIdentifier(string pUniqueIdentifier)
+        {
+            if (this._idValue == null)
+            {
+                this._idValue = Guid.Parse(pUniqueIdentifier).ToString();
+            }
+            else
+            {
+                throw new ArgumentException(@"Cannot overwrite an existing unique identifier for an object.");
+            }
+        }
+        public string IdValue { get => _idValue; }
+    }
+}
