00176c6f16ecf1ec7acb7a22de923224dc934ea9
Dany St-Laurent <dany.stlaurent1@gmail.com>
2021-04-11 14:05:51 -0400

    Integration of .netcore 3.1

--- IIOSWORLD-WEBAPITESTSERVER/com.iiosworld.service.webapi.TestServer/Startup.cs
+++ IIOSWORLD-WEBAPITESTSERVER/com.iiosworld.service.webapi.TestServer/Startup.cs
@@ -23,18 +23,16 @@ namespace com.iiosworld.service.webapi.TestServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(o => o.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.EnvironmentName.Equals("Developpement"))
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseMvc();
         }
     }
 }
